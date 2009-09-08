2f9fdb70364a726f89f7da2b8bfe2ab1bbdb6535
Michael Doube <michael@doube.net>
2009-09-08 12:03:16 +0100

    Add GPL v3 licence message to FitCircle

--- src/org/doube/bonej/FitCircle.java
+++ src/org/doube/bonej/FitCircle.java
@@ -1,5 +1,24 @@
 package org.doube.bonej;
 
+/**
+ * FitCircle Java class for fitting circles to 2D coordinate data
+ * 
+ * Copyright 2009 Michael Doube 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.util.Arrays;
 
 import ij.IJ;
@@ -136,7 +155,7 @@ public class FitCircle {
      * Taubin method, SVD version
      * 
      * @param points
-     * @return double[] containing (<i>x</i>, <i>y</i>) centre and radius 
+     * @return double[] containing (<i>x</i>, <i>y</i>) centre and radius
      */
     public double[] taubinSVD(double[][] points) {
 	int nPoints = points.length;
