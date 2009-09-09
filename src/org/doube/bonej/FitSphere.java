670f760e68509a419b819a0c91fd4fcda9fb854c
Michael Doube <michael@doube.net>
2009-09-09 15:16:37 +0100

    Add copyright notice

--- src/org/doube/bonej/FitSphere.java
+++ src/org/doube/bonej/FitSphere.java
@@ -1,5 +1,22 @@
 package org.doube.bonej;
 
+/**
+ * FitSphere class for ImageJ
+ * Copyright 2009 Michael Doube
+ * 
+ *This program is free software: you can redistribute it and/or modify
+ *it under the terms of the GNU General Public License as published by
+ *the Free Software Foundation, either version 3 of the License, or
+ *(at your option) any later version.
+ *
+ *This program is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *GNU General Public License for more details.
+ *
+ *You should have received a copy of the GNU General Public License
+ *along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import ij.gui.Roi;
 import ij.measure.Calibration;
 import ij.plugin.frame.RoiManager;
@@ -11,6 +28,16 @@ import java.awt.Rectangle;
 
 import Jama.Matrix;
 
+/**
+ *<p>
+ * Takes point selections from ROI manager and returns the centroid and radius
+ * of a best fit sphere Ported from Angelo Tardugno's C++
+ * </p>
+ * 
+ * 
+ *@author Michael Doube and Angelo Tardugno
+ *@version 0.1
+ */
 public class FitSphere {
 
     /**
