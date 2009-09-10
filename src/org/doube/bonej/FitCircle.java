81c645c98d495c14378a775108fe1b00759089d9
Michael Doube <michael@doube.net>
2009-09-10 14:21:13 +0100

    Remove commented MATLAB code

--- src/org/doube/bonej/FitCircle.java
+++ src/org/doube/bonej/FitCircle.java
@@ -741,49 +741,6 @@ public class FitCircle {
 	double[] centreRadius = { Par.get(0, 0), Par.get(0, 1), r };
 	return centreRadius;
     }
-//
-//    for iter=1:IterMAX         %  main loop, each run is one (main) iteration
-//
-//        while (1)         %  secondary loop - adjusting Lambda (no limit on cycles)
-//        
-//            DelPar = [J; lambda_sqrt*eye(2)]\[g; zeros(2,1)];   % step candidate
-//            progress = norm(DelPar)/(Radius+norm(Par)+epsilon);
-//            if (progress < epsilon)  break;  end;               % stopping rule
-//            ParTemp = Par - DelPar';
-//            [JTemp,gTemp,FTemp,RadiusTemp] = CurrentIteration(ParTemp,XY); objective function + derivatives
-//
-//            if (FTemp < F)                  %   yes, improvement
-//               lambda_sqrt = lambda_sqrt/2;   % reduce lambda, move to next iteration
-//               break;
-//            else                            %   no improvement
-//               lambda_sqrt = lambda_sqrt*2; % increase lambda, recompute the step
-//               continue;
-//            end
-//        end   %   while (1), the end of the secondary loop
-//        if (progress < epsilon)  break;  end;             % stopping rule
-//        Par = ParTemp;  J = JTemp;  g = gTemp;  F = FTemp;  Radius = RadiusTemp;  % update the iteration
-//    %    fprintf(1,' %3d   %.5f  %.5f\n',iter,Par);
-//    end    %  the end of the main loop (over iterations)
-//    Par = [Par , Radius];   % assembling the full parameter vector "Par" for output
-//    end    %    LMreduced
-//
-//    %================ function CurrentIteration ================
-//
-//    function [J,g,F,Radius] = CurrentIteration(Par,XY)
-//
-//    %    computes the objective function F and its derivatives at the current point Par
-//
-//    Dx = XY(:,1) - Par(1);
-//    Dy = XY(:,2) - Par(2);
-//    D = sqrt(Dx.*Dx + Dy.*Dy);
-//    Dx = Dx./D;  Dy = Dy./D;
-//    J = [-Dx+mean(Dx) , -Dy+mean(Dy)];
-//    Radius = mean(D);
-//    g = D - Radius;
-//    F = norm(g)^2;
-//
-//    end  %  CurrentIteration
-
     
     /**
      * Generate coordinates of a circle
