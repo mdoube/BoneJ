6364d9a503f2a4040dbd6ed16203f7eb9b3b60a8
Michael Doube <michael@doube.net>
2009-09-23 19:35:34 +0100

    Make DeleteSliceRange more generic, to handle ordinary stacks as well as virtual stacks.

---
+++ src/org/doube/bonej/DeleteSliceRange.java
@@ -0,0 +1,66 @@
+package org.doube.bonej;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.VirtualStack;
+import ij.gui.GenericDialog;
+import ij.plugin.PlugIn;
+
+public class DeleteSliceRange implements PlugIn {
+	public void run(String arg) {
+		ImagePlus imp = IJ.getImage();
+		if (null == imp) {
+			return;
+		}
+		GenericDialog gd = new GenericDialog("Delete slices");
+		gd.addMessage("Inclusive range of slices to delete");
+		gd.addNumericField("First", imp.getCurrentSlice(), 0);
+		gd.addNumericField("Last", imp.getCurrentSlice(), 0);
+		gd.showDialog();
+		if (gd.wasCanceled()) {
+			return;
+		}
+		int first = (int) Math.floor(gd.getNextNumber());
+		int last = (int) Math.floor(gd.getNextNumber());
+
+		if (imp.getStack().isVirtual()) {
+			VirtualStack stack = (VirtualStack) imp.getStack();
+			deleteSliceRange(stack, first, last);
+			imp.setStack(null, stack);
+		} else {
+			ImageStack stack = imp.getStack();
+			deleteSliceRange(stack, first, last);
+			imp.setStack(null, stack);
+		}
+		imp.show();
+	}
+
+	/**
+	 * Delete a range of slices from a stack
+	 * 
+	 * @param stack
+	 * @param first the first slice to remove
+	 * @param last the last slice to remove
+	 */
+	public void deleteSliceRange(ImageStack stack, int first, int last) {
+		for (int s = first; s <= last; s++) {
+			stack.deleteSlice(first);
+		}
+		return;
+	}
+
+	/**
+	 * Delete a range of slices from a virtual stack
+	 * 
+	 * @param stack
+	 * @param first the first slice to remove
+	 * @param last the last slice to remove
+	 */
+	public void deleteSliceRange(VirtualStack stack, int first, int last) {
+		for (int s = first; s <= last; s++) {
+			stack.deleteSlice(first);
+		}
+		return;
+	}
+}
