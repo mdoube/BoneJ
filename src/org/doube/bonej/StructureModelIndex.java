abf9738964b7db814a15acd4a072cde08eadb8c4
Michael Doube <michael@doube.net>
2010-03-11 10:57:03 +0000

    Add dialog to let users choose voxel resampling and mesh smoothing during calculation of SMI

--- src/org/doube/bonej/StructureModelIndex.java
+++ src/org/doube/bonej/StructureModelIndex.java
@@ -24,7 +24,9 @@ import javax.vecmath.Point3f;
 
 import ij.IJ;
 import ij.ImagePlus;
+import ij.gui.GenericDialog;
 import ij.plugin.PlugIn;
+import isosurface.MeshEditor;
 
 import marchingcubes.MCTriangulator;
 
@@ -61,7 +63,19 @@ public class StructureModelIndex implements PlugIn {
 			IJ.error("SMI needs a binary image.");
 			return;
 		}
-		double smi = skyScan(imp);
+
+		GenericDialog gd = new GenericDialog("Mesh Parameters");
+		gd.addNumericField("Voxel resampling", 6, 0, 5, "voxels");
+		gd.addNumericField("Mesh smoothing (0-1)", 0.5, 3, 5, "");
+		gd.showDialog();
+		if (gd.wasCanceled()) {
+			return;
+		}
+
+		int voxelResampling = (int) Math.floor(gd.getNextNumber());
+		float meshSmoothing = (float) gd.getNextNumber();
+
+		double smi = skyScan(imp, voxelResampling, meshSmoothing);
 		ResultInserter ri = ResultInserter.getInstance();
 		ri.setResultInRow(imp, "SMI", smi);
 		ri.updateTable();
@@ -79,36 +93,50 @@ public class StructureModelIndex implements PlugIn {
 	 * 
 	 * @param imp
 	 *            binary ImagePlus
+	 * @param voxelResampling
+	 *            resampling of voxels for smoother mesh creation. If set too
+	 *            large will obliterate small features, if set too small, will
+	 *            get too many surface triangles with jagging artefact.
+	 * @param meshSmoothing
+	 *            factor between 0 and 1 for smoothing of the surface mesh
 	 * @return SMI. This is 0 for a plate, 3 for a rod and 4 for a sphere. SMI
 	 *         can be negative if the structure contains many concave surfaces.
 	 * @see <a href="http://www.skyscan.be/products/downloads.htm">The
 	 *      description of measured parameters</a>
 	 */
 	@SuppressWarnings("unchecked")
-	public static double skyScan(ImagePlus imp) {
+	public static double skyScan(ImagePlus imp, int voxelResampling,
+			float meshSmoothing) {
 		int threshold = 128;
 		final boolean[] channels = { true, false, false };
-		int resamplingF = 6;
 		MCTriangulator mct = new MCTriangulator();
 		IJ.showStatus("Finding surface points...");
 		List<Point3f> points = mct.getTriangles(imp, threshold, channels,
-				resamplingF);
+				voxelResampling);
 		final Color3f colour = new Color3f(0.0f, 0.0f, 0.0f);
 		IJ.showStatus("Creating surface mesh...");
 		CustomTriangleMesh surface = new CustomTriangleMesh(points, colour,
 				0.0f);
+		IJ.showStatus("Smoothing surface mesh...");
+		MeshEditor.smooth(surface, meshSmoothing);
 		IJ.showStatus("Calculating volume...");
 		double v = surface.getVolume();
 
-		double s1 = MeasureSurface.getSurfaceArea(points);
+		double s1 = MeasureSurface.getSurfaceArea(surface.getMesh());
 
 		IJ.showStatus("Dilating voxel model...");
 		Dilate d = new Dilate();
 		ImagePlus imp2 = d.dilate(imp, 255);
 
 		IJ.showStatus("Finding surface points...");
-		points = mct.getTriangles(imp2, threshold, channels, resamplingF);
-		double s2 = MeasureSurface.getSurfaceArea(points);
+		points = mct.getTriangles(imp2, threshold, channels, voxelResampling);
+		imp2.changes = false;
+		imp2.close();
+		IJ.showStatus("Creating surface mesh...");
+		surface = new CustomTriangleMesh(points, colour, 0.0f);
+		IJ.showStatus("Smoothing surface mesh...");
+		MeshEditor.smooth(surface, meshSmoothing);
+		double s2 = MeasureSurface.getSurfaceArea(surface.getMesh());
 		double smi = 6 * ((s2 - s1) * v / (s1 * s1));
 		IJ.showStatus("SMI calculated.");
 		return smi;
