6694b009ba4eff3666d6dba3ea1d640de758f6b8
Michael Doube <michael@doube.net>
2010-04-01 12:04:12 +0100

    Tidy up BoneList

--- src/org/doube/bonej/BoneList.java
+++ src/org/doube/bonej/BoneList.java
@@ -6,60 +6,54 @@ import java.util.regex.Pattern;
 import ij.ImagePlus;
 
 /**
- * Utility methods for making lists of bones and guessing which bone is in an image 
+ * Utility methods for making lists of bones and guessing which bone is in an
+ * image
  * 
  * @author Michael Doube
- *
+ * 
  */
 public class BoneList {
+
 	/**
-	 * Return an array of bone names
+	 * List of bone names
+	 */
+	// Only add new bone names to the END of this list
+	private final static String[] boneList = { "unknown", "scapula", "humerus",
+			"radius", "ulna", "metacarpal", "pelvis", "femur", "tibia",
+			"fibula", "metatarsal", "calcaneus", "tibiotarsus",
+			"tarsometatarsal" };
+
+	/**
+	 * Return the array of bone names
 	 * 
 	 * @return array of bone names
 	 */
-	public final static String[] getBoneList(){
-		//ONLY ADD TO THE END OF THIS ARRAY
-		String[] boneList = {
-			"unknown",
-			"scapula",
-			"humerus",
-			"radius",
-			"ulna",
-			"metacarpal",
-			"pelvis",
-			"femur",
-			"tibia",
-			"fibula",
-			"metatarsal",
-			"calcaneus",
-			"tibiotarsus",
-			"tarsometatarsal"
-			};
+	public final static String[] get() {
 		return boneList;
 	}
-	
+
 	/**
 	 * Guess from the image title which bone is in the image
 	 * 
 	 * @param imp
-	 * @return integer code relating to the position of the bone's name in the bone list
+	 * @return integer code relating to the position of the bone's name in the
+	 *         bone list
 	 */
-	public int guessBone(ImagePlus imp){
+	public int guessBone(ImagePlus imp) {
 		String boneString = imp.getTitle();
 		return guessBone(boneString);
 	}
-	
+
 	/**
 	 * Return the boneID of a bone in boneList that matches the input string
 	 * 
 	 * @param boneString
 	 * @return
 	 */
-	public int guessBone(String boneString){
-		String[] bones = getBoneList();
+	public int guessBone(String boneString) {
 		int boneID = 0;
-		for (int n = 0; n < bones.length; n++) {
-			Pattern p = Pattern.compile(bones[n], Pattern.CASE_INSENSITIVE);
+		for (int n = 0; n < boneList.length; n++) {
+			Pattern p = Pattern.compile(boneList[n], Pattern.CASE_INSENSITIVE);
 			Matcher m = p.matcher(boneString);
 			if (m.find()) {
 				boneID = n;
