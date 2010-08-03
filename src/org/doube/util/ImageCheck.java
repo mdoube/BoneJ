2147599436e42948fe6a954cd1902a66710ce172
Michael Doube <michael@doube.net>
2010-08-03 20:22:00 +0100

    Require ImageJ 1.44e due to a bug fix to autothresholding

--- src/org/doube/util/ImageCheck.java
+++ src/org/doube/util/ImageCheck.java
@@ -17,7 +17,7 @@ public class ImageCheck {
 	/**
 	 * ImageJ version required by BoneJ
 	 */
-	public static final String requiredIJVersion = "1.44a";
+	public static final String requiredIJVersion = "1.44e";
 
 	/**
 	 * Check if image is binary
