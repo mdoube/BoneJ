6c0dc513895833385a10cafd9c8b4ffb3204f2f0
Michael Doube <michael@doube.net>
2010-08-30 14:33:50 +0100

    Copy-paste from command line to avoid Eclipse jiggery-pokery

--- src/org/doube/skeleton/AnalyzeSkeleton_.java
+++ src/org/doube/skeleton/AnalyzeSkeleton_.java
@@ -1,4 +1,4 @@
-package org.doube.skeleton;
+package skeleton_analysis;
 
 import java.util.ArrayList;
 import java.util.Collections;
--- src/org/doube/skeleton/Graph.java
+++ src/org/doube/skeleton/Graph.java
@@ -1,4 +1,4 @@
-package org.doube.skeleton;
+package skeleton_analysis;
 
 import ij.IJ;
 
