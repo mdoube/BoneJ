f3403ea221c002117596e878295d3c40635aded7
Michael Doube <michael@doube.net>
2011-03-08 14:23:15 +0000

    RoiMan: Add method to make a cropped copy of a stack based on ROIs ROI Manager

--- src/org/doube/util/RoiMan.java
+++ src/org/doube/util/RoiMan.java
@@ -1,9 +1,11 @@
 package org.doube.util;
 
 import ij.ImagePlus;
+import ij.ImageStack;
 import ij.gui.Roi;
 import ij.measure.Calibration;
 import ij.plugin.frame.RoiManager;
+import ij.process.ImageProcessor;
 
 import java.awt.List;
 import java.awt.Rectangle;
@@ -53,8 +55,8 @@ public class RoiMan {
 	}
 
 	/**
-	 * Return a list of ROIs that are active in the given slice, s.
-	 * ROIs without a slice number are assumed to be active in all slices.
+	 * Return a list of ROIs that are active in the given slice, s. ROIs without
+	 * a slice number are assumed to be active in all slices.
 	 * 
 	 * @param roiMan
 	 * @param s
@@ -63,7 +65,7 @@ public class RoiMan {
 	public static ArrayList<Roi> getSliceRoi(RoiManager roiMan, int s) {
 		ArrayList<Roi> roiList = new ArrayList<Roi>();
 		Roi[] rois = roiMan.getRoisAsArray();
-		for (Roi roi : rois){
+		for (Roi roi : rois) {
 			int sliceNumber = roiMan.getSliceNumber(roi.getName());
 			if (sliceNumber == s || sliceNumber == -1)
 				roiList.add(roi);
@@ -77,8 +79,8 @@ public class RoiMan {
 	 * @param roiMan
 	 * @return int[] containing x min, x max, y min, y max, z min and z max, or
 	 *         null if there is no ROI Manager or if the ROI Manager is empty.
-	 *         If any of the ROIs contains no slice information, z min is set to 1
-	 *         and z max is set to Integer.MAX_VALUE
+	 *         If any of the ROIs contains no slice information, z min is set to
+	 *         1 and z max is set to Integer.MAX_VALUE
 	 */
 	public static int[] getLimits(RoiManager roiMan) {
 		if (roiMan == null || roiMan.getCount() == 0)
@@ -102,7 +104,7 @@ public class RoiMan {
 				zmin = Math.min(slice, zmin);
 				zmax = Math.max(slice, zmax);
 			} else
-				noZroi = true; //found a ROI with no Z info
+				noZroi = true; // found a ROI with no Z info
 		}
 		if (noZroi) {
 			int[] limits = { xmin, xmax, ymin, ymax, 1, Integer.MAX_VALUE };
@@ -111,5 +113,56 @@ public class RoiMan {
 			int[] limits = { xmin, xmax, ymin, ymax, zmin, zmax };
 			return limits;
 		}
+	}
+
+	/**
+	 * Crop a stack to the limits of the ROIs in the ROI Manager and optionally
+	 * fill the background with a single pixel value.
+	 * 
+	 * @param roiMan
+	 * @param stack
+	 * @param fillBackground
+	 * @return cropped copy of input stack
+	 */
+	public static ImageStack cropStack(RoiManager roiMan, ImageStack stack,
+			boolean fillBackground, int fillValue) {
+		int[] limits = getLimits(roiMan);
+		final int xmin = limits[0];
+		final int xmax = limits[1];
+		final int ymin = limits[2];
+		final int ymax = limits[3];
+		final int zmin = limits[4];
+		final int zmax = (limits[5] == Integer.MAX_VALUE) ? stack.getSize()
+				: limits[5];
+		final int w = xmax - xmin;
+		final int h = ymax - ymin;
+
+		ImageStack out = new ImageStack(w, h);
+		for (int z = zmin; z <= zmax; z++) {
+			ImageProcessor ip = stack.getProcessor(z);
+			ImageProcessor ipOut = ip.duplicate();
+			final int length = ipOut.getPixelCount();
+			if (fillBackground)
+				for (int i = 0; i < length; i++)
+					ipOut.set(i, fillValue);
+			ArrayList<Roi> rois = getSliceRoi(roiMan, z);
+			for (Roi roi : rois) {
+				ip.setRoi(roi);
+				Rectangle r = roi.getBounds();
+				ImageProcessor mask = ip.getMask();
+				final int rh = r.y + r.height;
+				final int rw = r.x + r.width;
+				for (int y = r.y; y < rh; y++) {
+					for (int x = r.x; x < rw; x++) {
+						if (mask.get(x - r.x, y - r.y) > 0)
+							ipOut.set(x, y, ip.get(x, y));
+					}
+				}
+			}
+			ipOut.setRoi(xmin, ymin, w, h);
+			ipOut = ipOut.crop();
+			out.addSlice(stack.getSliceLabel(z), ipOut);
+		}
+		return out;
 	}
 }
