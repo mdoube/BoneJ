e560437b7c6248571c40eeb28e618bde0462f46f
Michael Doube <michael@doube.net>
2011-04-14 13:52:21 +0100

    StackCropper: Show options to user in a GenericDialog

--- src/org/doube/util/StackCropper.java
+++ src/org/doube/util/StackCropper.java
@@ -3,6 +3,7 @@ package org.doube.util;
 import ij.ImagePlus;
 import ij.ImageStack;
 import ij.WindowManager;
+import ij.gui.GenericDialog;
 import ij.plugin.PlugIn;
 import ij.plugin.frame.RoiManager;
 
@@ -20,10 +21,27 @@ public class StackCropper implements PlugIn {
 		if (imp == null || roiMan == null)
 			return;
 
-		ImageStack stack = RoiMan.cropStack(roiMan, imp.getImageStack(), false,
-				0, 0);
-		ImagePlus out = new ImagePlus(imp.getTitle()+"-crop");
-		out.setStack(stack);
-		out.show();
+		GenericDialog gd = new GenericDialog("Crop Stack by ROI");
+		gd.addCheckbox("Replace Original", false);
+		gd.addCheckbox("Fill outside", false);
+		gd.addNumericField("Fill_value", 0, 0, 6, "");
+		gd.addNumericField("Padding", 0, 0);
+		gd.showDialog();
+		if (gd.wasCanceled())
+			return;
+		boolean doReplace = gd.getNextBoolean();
+		boolean doFill = gd.getNextBoolean();
+		int fillValue = (int) gd.getNextNumber();
+		int padding = (int) gd.getNextNumber();
+		ImageStack stack = RoiMan.cropStack(roiMan, imp.getImageStack(),
+				doFill, fillValue, padding);
+		if (doReplace) {
+			imp.setStack(stack);
+			imp.show();
+		} else {
+			ImagePlus out = new ImagePlus(imp.getTitle() + "-crop");
+			out.setStack(stack);
+			out.show();
+		}
 	}
 }
