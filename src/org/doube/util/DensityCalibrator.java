641a5c9ce9cf1a91a858141756e412d418318e99
Michael Doube <michael@doube.net>
2011-07-22 14:29:00 +0200

    Improve error / exception handling

--- src/org/doube/util/DensityCalibrator.java
+++ src/org/doube/util/DensityCalibrator.java
@@ -14,18 +14,26 @@ public class DensityCalibrator implements PlugIn {
 		if (arg.equals("scanco"))
 			try {
 				scanco(imp);
-			} catch (Exception e) {
+			} catch (NumberFormatException e) {
+				IJ.error("Calibration data missing from DICOM header");
+				return;
+			} catch (NullPointerException e) {
+				IJ.error("Calibration data missing from DICOM header");
+				return;
+			} catch (IllegalArgumentException e) {
 				IJ.error(e.getMessage());
+				return;
+			} catch (Exception e){
+				IJ.error("Can't calibrate image\n"+e.getMessage());
 				return;
 			}
 	}
 
 	private void scanco(ImagePlus imp) throws IllegalArgumentException {
 		String manufacturer = DicomTools.getTag(imp, "0008,0070");
-		IJ.log("manufacturer = "+manufacturer);
-		if (!manufacturer.contains("SCANCO")) {
+		if (manufacturer == null || !manufacturer.contains("SCANCO")) {
 			throw new IllegalArgumentException(
-					"File not a SCANCO Medical DICOM");
+					"File is not a SCANCO Medical DICOM");
 		}
 		double slope = Double.parseDouble(DicomTools.getTag(imp, "0029,1004"));
 		double intercept = Double.parseDouble(DicomTools.getTag(imp,
@@ -34,7 +42,7 @@ public class DensityCalibrator implements PlugIn {
 				.parseDouble(DicomTools.getTag(imp, "0029,1000"));
 		double c = intercept - 32768 * slope / scaling;
 		double m = slope / scaling;
-		double[] coef = {c, m};
+		double[] coef = { c, m };
 		Calibration cal = imp.getCalibration();
 		cal.setFunction(Calibration.STRAIGHT_LINE, coef, "mg HA/ccm");
 		imp.setCalibration(cal);
