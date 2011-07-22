c938cc027b87d4b18a873cf457a116704413178a
Michael Doube <michael@doube.net>
2011-07-22 10:05:44 +0200

    New class to apply density calibration to images

---
+++ src/org/doube/util/DensityCalibrator.java
@@ -0,0 +1,42 @@
+package org.doube.util;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.WindowManager;
+import ij.measure.Calibration;
+import ij.plugin.PlugIn;
+import ij.util.DicomTools;
+
+public class DensityCalibrator implements PlugIn {
+
+	@Override
+	public void run(String arg) {
+		ImagePlus imp = WindowManager.getCurrentImage();
+		if (arg.equals("scanco"))
+			try {
+				scanco(imp);
+			} catch (Exception e) {
+				IJ.error(e.getMessage());
+				return;
+			}
+	}
+
+	private void scanco(ImagePlus imp) throws IllegalArgumentException {
+		String manufacturer = DicomTools.getTag(imp, "0008,0070");
+		if (!manufacturer.equals("SCANCO Medical")) {
+			throw new IllegalArgumentException(
+					"File not a SCANCO Medical DICOM");
+		}
+		double slope = Double.parseDouble(DicomTools.getTag(imp, "0029,1004"));
+		double intercept = Double.parseDouble(DicomTools.getTag(imp,
+				"0029,1005"));
+		double scaling = Double
+				.parseDouble(DicomTools.getTag(imp, "0029,1000"));
+		double c = intercept - 32768 * slope / scaling;
+		double m = slope / scaling;
+		double[] coef = {c, m};
+		Calibration cal = imp.getCalibration();
+		cal.setFunction(Calibration.STRAIGHT_LINE, coef, "mg HA/ccm");
+	}
+
+}
