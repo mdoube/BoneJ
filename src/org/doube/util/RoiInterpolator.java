fcee4d4e7fca8d17d5c1743d1b1b14a725e767f9
Michael Doube <michael@doube.net>
2011-11-06 08:56:20 +0100

    Revert "RoiInterpolator: try using copy-pasted EDT code (doesn't work)"
    This reverts commit 647e2572a7b992327ca5540c20694f2ed1fc06fe.

--- src/org/doube/util/RoiInterpolator.java
+++ src/org/doube/util/RoiInterpolator.java
@@ -67,13 +67,9 @@ public class RoiInterpolator implements PlugIn {
 			}
 			stack.addSlice(""+s, bp);
 		}
-		//do the binary interpolation with IDT
-//		run(stack);
-//		ImagePlus binary = new ImagePlus("interpolated", stack);
-		
-		//do the binary interpolation with EDT
-		ImagePlus binary = compute(stack);
-		binary.show();
+		//do the binary interpolation
+		run(stack);
+		ImagePlus binary = new ImagePlus("interpolated", stack);
 
 		//get the ROIs
 		ThresholdToSelection ts = new ThresholdToSelection();
