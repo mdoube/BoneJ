698d0c645cd2ece77174de4e629400eb35a82c51
Michael Doube <michael@doube.net>
2011-11-30 16:00:09 +0100

    MeasureSurfaceTest: don't test the plugin's run() method

--- test/org/doube/bonej/MeasureSurfaceTest.java
+++ test/org/doube/bonej/MeasureSurfaceTest.java
@@ -8,11 +8,6 @@ import org.junit.Test;
 public class MeasureSurfaceTest {
 
 	@Test
-	public void testRun() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
 	public void testGetSurfaceArea() {
 		double area = MeasureSurface.getSurfaceArea(TestDataMaker.octahedron());
 		assertEquals(4.4558146404, area, 1e-6);
