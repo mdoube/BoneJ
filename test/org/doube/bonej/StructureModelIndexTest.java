50f6cd4cfd19431c8a1a46d8b5d4e26364719b2c
Michael Doube <michael@doube.net>
2011-11-30 17:05:31 +0100

    Add unit test for SMI calculation (rods only so far)

---
+++ test/org/doube/bonej/StructureModelIndexTest.java
@@ -0,0 +1,29 @@
+package org.doube.bonej;
+
+import static org.junit.Assert.*;
+import ij.ImagePlus;
+
+import org.doube.geometry.TestDataMaker;
+import org.junit.Test;
+
+public class StructureModelIndexTest {
+
+	@Test
+	public void testHildRuegRod() {
+		int length = 16384;
+		int diameter = 64;
+		ImagePlus imp = TestDataMaker.rod(length, diameter);
+		double smi = StructureModelIndex.hildRueg(imp, 6, 0.5f);
+		assertEquals(3.0, smi, 0.03);
+	}
+	
+	@Test
+	public void testHildRuegPlate() {
+		//TODO not yet implemented
+	}
+	
+	@Test
+	public void testHildRuegSphere() {
+		//TODO not yet implemented
+	}
+}
