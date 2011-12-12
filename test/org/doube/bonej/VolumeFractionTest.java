95666c7272ed65d6ac55097544e8d1f9f2d5b01a
Michael Doube <michael@doube.net>
2011-12-12 15:01:07 +0100

    Use precise expected values for voxel mode test

--- test/org/doube/bonej/VolumeFractionTest.java
+++ test/org/doube/bonej/VolumeFractionTest.java
@@ -1,7 +1,12 @@
 package org.doube.bonej;
 
 import static org.junit.Assert.*;
+
+import java.awt.Rectangle;
+
 import ij.ImagePlus;
+import ij.gui.Roi;
+import ij.plugin.frame.RoiManager;
 
 import org.doube.geometry.TestDataMaker;
 import org.junit.Test;
@@ -11,19 +16,19 @@ public class VolumeFractionTest {
 	private final ImagePlus sphere = TestDataMaker.sphere(64);
 	private final ImagePlus brick = TestDataMaker.brick(32, 64, 96);
 	private VolumeFraction vf = new VolumeFraction();
-	private final double[] expectedRod = { 256 * Math.PI * 32 * 32,
+	private final double[] expectedRod = { 826368,
 			256 * 128 * 128 };
-	private final double[] expectedSphere = { 4 * Math.PI * 64 * 64 * 64 / 3,
+	private final double[] expectedSphere = { 1097342,
 			130 * 130 * 131 };
 	private final double[] expectedBrick = { 32 * 64 * 96, 34 * 66 * 98 };
 
 	@Test
 	public void testGetVolumesImagePlusDoubleDouble() {
 		double[] vols = vf.getVolumes(rod, 1, 255);
-		assertArrayEquals(expectedRod, vols, 3000);
+		assertArrayEquals(expectedRod, vols, 0);
 
 		vols = vf.getVolumes(sphere, 1, 255);
-		assertArrayEquals(expectedSphere, vols, 2000);
+		assertArrayEquals(expectedSphere, vols, 0);
 
 		vols = vf.getVolumes(brick, 1, 255);
 		assertArrayEquals(expectedBrick, vols, 0);
@@ -31,7 +36,10 @@ public class VolumeFractionTest {
 
 	@Test
 	public void testGetVolumesImagePlusDoubleDoubleBoolean() {
-		fail("Not yet implemented"); // TODO
+		RoiManager roiMan = new RoiManager();
+		int w = rod.getWidth();
+		roiMan.addRoi(new Roi(new Rectangle(0, 0, w / 2, w / 2)));
+
 	}
 
 	@Test
