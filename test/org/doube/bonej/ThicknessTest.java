a53ecab0ed81af8ae585f96134413d1bef2e1756
Michael Doube <michael@doube.net>
2011-12-13 15:51:08 +0100

    Add test for Thickness using brick primitive shape
    Interesting pixel effect revealed here where bricks which are odd number
    of pixels thick are measured as t+1. Probably a pixelation effect
    exacerbated by Thickness code which multiplies measured radius by 2.
    Possibly not an issue for real image data, but that remains to be
    tested.

--- test/org/doube/bonej/ThicknessTest.java
+++ test/org/doube/bonej/ThicknessTest.java
@@ -19,17 +19,32 @@ public class ThicknessTest {
 			assertEquals(d, stats[0], 1.5);
 		}
 	}
-	
+
 	@Test
 	public void testGetLocalThicknessSphere() {
 		Thickness th = new Thickness();
-		for (int r = 2; r < 25; r += 1) {
+		for (int r = 2; r < 25; r++) {
 			ImagePlus sphere = TestDataMaker.sphere(r);
 			ImagePlus imp = th.getLocalThickness(sphere, false);
 			double[] stats = StackStats.meanStdDev(imp);
-			System.out.print(r+", "+stats[0]+"\n");
-			double regression = r*1.9441872882 - 1.218936;
+			double regression = r * 1.9441872882 - 1.218936;
 			assertEquals(regression, stats[0], regression * 0.1);
+		}
+	}
+
+	@Test
+	public void testGetLocalThicknessBrick() {
+		Thickness th = new Thickness();
+		for (int t = 1; t < 21; t++) {
+			ImagePlus brick = TestDataMaker.brick(128, 128, t);
+			ImagePlus imp = th.getLocalThickness(brick, false);
+			double[] stats = StackStats.meanStdDev(imp);
+			int expected = t;
+			//pixelation and *2 (radius to diameter conversion) weirdness
+			if (t%2 != 0)
+				expected++;
+			System.out.print(t + ", " + stats[0] + "\n");
+			assertEquals(expected, stats[0], expected * 0.05);
 		}
 	}
 
