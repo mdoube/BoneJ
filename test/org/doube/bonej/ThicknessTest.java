b61dac6a9c01cbd6372b56c27d53774f1a5b2782
Michael Doube <michael@doube.net>
2011-12-13 14:59:19 +0100

    Add test for Thickness using spheres of different sizes
    Similar to small diameter rods, small spheres suffer from pixellation
    artefact and there is a constant bias due to the ambiguity inherent in
    measuring distances in discretised data. There is a very high
    correlation (R^2 > 0.999) between synthetic sphere diameter and measured
    diameter, however.

--- test/org/doube/bonej/ThicknessTest.java
+++ test/org/doube/bonej/ThicknessTest.java
@@ -10,13 +10,26 @@ import org.junit.Test;
 public class ThicknessTest {
 
 	@Test
-	public void testGetLocalThickness() {
+	public void testGetLocalThicknessRod() {
 		Thickness th = new Thickness();
 		for (int d = 1; d < 25; d += 1) {
 			ImagePlus rod = TestDataMaker.rod(d * 100, d);
 			ImagePlus imp = th.getLocalThickness(rod, false);
 			double[] stats = StackStats.meanStdDev(imp);
 			assertEquals(d, stats[0], 1.5);
+		}
+	}
+	
+	@Test
+	public void testGetLocalThicknessSphere() {
+		Thickness th = new Thickness();
+		for (int r = 2; r < 25; r += 1) {
+			ImagePlus sphere = TestDataMaker.sphere(r);
+			ImagePlus imp = th.getLocalThickness(sphere, false);
+			double[] stats = StackStats.meanStdDev(imp);
+			System.out.print(r+", "+stats[0]+"\n");
+			double regression = r*1.9441872882 - 1.218936;
+			assertEquals(regression, stats[0], regression * 0.1);
 		}
 	}
 
