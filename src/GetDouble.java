a452ac60699269aa40fdef5ba9ad53f932f12731
Timo Rantalainen <tjrantal@gmail.com>
2012-01-16 13:30:59 +1100

    Added get double plugin to be used in macros in case the calibration needs to be read from a file (e.g. DICOM files)

---
+++ src/GetDouble.java
@@ -0,0 +1,8 @@
+import ij.*;
+/*Compile javac -cp ".;C:\Oma\sourcet\ImageJ_testing\ij.jar" GetDouble.java*/
+public class GetDouble{
+	public static double getDouble(String arg1) {
+		return Double.valueOf(arg1);
+	}
+
+}
