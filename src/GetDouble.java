811e5edba0fcd7d0349f5542d9c38a35b53d81de
Timo Rantalainen <tjrantal@gmail.com>
2012-02-17 09:55:48 +1100

    Removed the redundant GetDouble class

--- src/GetDouble.java
+++
@@ -1,8 +0,0 @@
-import ij.*;
-/*Compile javac -cp ".;C:\Oma\sourcet\ImageJ_testing\ij.jar" GetDouble.java*/
-public class GetDouble{
-	public static double getDouble(String arg1) {
-		return Double.valueOf(arg1);
-	}
-
-}
