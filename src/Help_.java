1e86c20dda818ff874553985803109dbc633a661
Michael Doube <michael@doube.net>
2012-03-07 17:07:49 +0100

    BoneJ version 1.3.4

--- src/Help_.java
+++ src/Help_.java
@@ -17,7 +17,7 @@ public class Help_ implements PlugIn {
 	/**
 	 * BoneJ version
 	 */
-	public static final String bonejVersion = "1.3.3";
+	public static final String bonejVersion = "1.3.4";
 
 	public void run(String arg) {
 		if (arg.equals("about")) {
