c55933d4b43472921ad8b282652dbb14d106f57e
Michael Doube <michael@doube.net>
2012-04-24 22:57:51 +0200

    SkeletonAngles: allow calculation of angle at any point along edge

--- src/org/bonej/SkeletonAngles.java
+++ src/org/bonej/SkeletonAngles.java
@@ -14,9 +14,12 @@ import org.doube.util.UsageReporter;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.WindowManager;
+import ij.gui.GenericDialog;
 import ij.plugin.PlugIn;
 
 public class SkeletonAngles implements PlugIn {
+
+	private static final int VERTEX_TO_VERTEX = -1;
 
 	public void run(String arg) {
 		ImagePlus imp = WindowManager.getCurrentImage();
@@ -24,15 +27,33 @@ public class SkeletonAngles implements PlugIn {
 			IJ.noImage();
 			return;
 		}
-		
+
+		GenericDialog gd = new GenericDialog("Options");
+		String[] options = { "Opposite edges", "Vertex and nth edge pixel" };
+		gd.addChoice("Measure angle between...", options, options[0]);
+		gd.addNumericField("nth pixel", 0, 0);
+
+		gd.showDialog();
+
+		if (gd.wasCanceled())
+			return;
+
+		int choice = gd.getNextChoiceIndex();
+
+		int nthPixel = (int) Math.floor(gd.getNextNumber());
+
+		if (choice == 0) {
+			nthPixel = VERTEX_TO_VERTEX;
+		}
+
 		AnalyzeSkeleton skeletonAnalyzer = new AnalyzeSkeleton();
 		skeletonAnalyzer.setup("", imp);
 		skeletonAnalyzer.run();
-		
+
 		ResultInserter ri = ResultInserter.getInstance();
-		
+
 		Graph[] graphs = skeletonAnalyzer.getGraphs();
-		int g = 0; 
+		int g = 0;
 		int v = 0;
 		for (Graph graph : graphs) {
 			ArrayList<Vertex> vertices = graph.getVertices();
@@ -52,15 +73,15 @@ public class SkeletonAngles implements PlugIn {
 					Edge edge1 = edges.get(1);
 					Edge edge2 = edges.get(2);
 
-					double theta01 = vertexAngle(vertex, edge0, edge1);
-					double theta02 = vertexAngle(vertex, edge0, edge2);
-					double theta12 = vertexAngle(vertex, edge1, edge2);
-					
+					double theta01 = vertexAngle(vertex, edge0, edge1, nthPixel);
+					double theta02 = vertexAngle(vertex, edge0, edge2, nthPixel);
+					double theta12 = vertexAngle(vertex, edge1, edge2, nthPixel);
+
 					ri.setResultInRow(imp, "Skeleton", g);
 					ri.setResultInRow(imp, "Vertex", v);
 					ri.setResultInRow(imp, "Theta 0", theta01);
 					ri.setResultInRow(imp, "Theta 1", theta02);
-					ri.setResultInRow(imp, "Theta 2", theta12);	
+					ri.setResultInRow(imp, "Theta 2", theta12);
 				}
 				v++;
 			}
@@ -76,24 +97,72 @@ public class SkeletonAngles implements PlugIn {
 		ArrayList<Point> pointsv = vertex.getPoints();
 		ArrayList<Point> points0 = v0.getPoints();
 		ArrayList<Point> points1 = v1.getPoints();
-		double[] centroidv = calculateCentroid(pointsv);
-		double[] centroid0 = calculateCentroid(points0);
-		double[] centroid1 = calculateCentroid(points1);
+		double[] cv = calculateCentroid(pointsv);
+		double[] c0 = calculateCentroid(points0);
+		double[] c1 = calculateCentroid(points1);
 
-		double x0 = centroid0[0] - centroidv[0];
-		double y0 = centroid0[1] - centroidv[1];
-		double z0 = centroid0[2] - centroidv[2];
-		double x1 = centroid1[0] - centroidv[0];
-		double y1 = centroid1[1] - centroidv[1];
-		double z1 = centroid1[2] - centroidv[2];
+		return Trig.angle3D(c0[0], c0[1], c0[2], c1[0], c1[1], c1[2], cv[0],
+				cv[1], cv[2]);
 
-		double dot = x0 * x1 + y0 * y1 + z0 * z1;
-		double d0 = Trig.distance3D(centroidv, centroid0);
-		double d1 = Trig.distance3D(centroidv, centroid1);
+		// double x0 = centroid0[0] - centroidv[0];
+		// double y0 = centroid0[1] - centroidv[1];
+		// double z0 = centroid0[2] - centroidv[2];
+		// double x1 = centroid1[0] - centroidv[0];
+		// double y1 = centroid1[1] - centroidv[1];
+		// double z1 = centroid1[2] - centroidv[2];
+		//
+		// double dot = x0 * x1 + y0 * y1 + z0 * z1;
+		// double d0 = Trig.distance3D(centroidv, centroid0);
+		// double d1 = Trig.distance3D(centroidv, centroid1);
+		//
+		// double cosTheta = dot / (d0 * d1);
+		//
+		// return Math.acos(cosTheta);
+	}
 
-		double cosTheta = dot / (d0 * d1);
-		
-		return Math.acos(cosTheta);
+	private double vertexAngle(Vertex vertex, Edge edge0, Edge edge1,
+			int nthPoint) {
+		if (nthPoint == VERTEX_TO_VERTEX)
+			return vertexAngle(vertex, edge0, edge1);
+		Point p0 = getNthPoint(vertex, edge0, nthPoint);
+		Point p1 = getNthPoint(vertex, edge1, nthPoint);
+		double[] cv = calculateCentroid(vertex.getPoints());
+
+		return Trig.angle3D(p0.x, p0.y, p0.z, p1.x, p1.y, p1.z, cv[0], cv[1],
+				cv[2]);
+	}
+
+	private Point getNthPoint(Vertex vertex, Edge edge, int nthPoint) {
+		ArrayList<Point> vertexPoints = vertex.getPoints();
+		ArrayList<Point> edgePoints = edge.getSlabs();
+		boolean startAtZero = false;
+		outerloop: for (Point v : vertexPoints) {
+			Point p0 = edgePoints.get(0);
+			for (int x = v.x - 1; x <= v.x + 1; x++) {
+				for (int y = v.y - 1; y <= v.y + 1; y++) {
+					for (int z = v.z - 1; z <= v.z + 1; z++) {
+						if (x == 0 && y == 0 && z == 0)
+							continue;
+						if (x == p0.x && y == p0.y && z == p0.z) {
+							startAtZero = true;
+							break outerloop;
+						}
+					}
+				}
+			}
+		}
+
+		if (startAtZero) {
+			if (nthPoint < edgePoints.size())
+				return edgePoints.get(nthPoint);
+			else
+				return edgePoints.get(edgePoints.size() - 1);
+		} else {
+			if (nthPoint < edgePoints.size())
+				return edgePoints.get(edgePoints.size() - nthPoint - 1);
+			else
+				return edgePoints.get(0);
+		}
 	}
 
 	private double[] calculateCentroid(ArrayList<Point> points) {
