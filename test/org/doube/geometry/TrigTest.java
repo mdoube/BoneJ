ff457118782e83e69a01bd6832f8175c526fc8ce
Michael Doube <michael@doube.net>
2012-04-26 16:26:27 +0200

    TrigTest: add test for angle3D() method

--- test/org/doube/geometry/TrigTest.java
+++ test/org/doube/geometry/TrigTest.java
@@ -36,5 +36,15 @@ public class TrigTest {
 		double expected = Math.sqrt(77);
 		assertEquals(expected, result, 1e-12);
 	}
+	
+	@Test
+	public void testAngle3D(){
+		double result = Trig.angle3D(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		double expected = 0;
+		assertEquals(expected, result, 1e-12);
+		result = Trig.angle3D(1, 2, 3, 4, 5, 6, 0, 0, 0);
+		expected = Math.acos(32 / (Math.sqrt(14) * Math.sqrt(77)));
+		assertEquals(expected, result, 1e-12);
+	}
 
 }
