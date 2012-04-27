d2934dc57f1dc89e6910d2b730f0471cfec272b6
Michael Doube <michael@doube.net>
2012-04-27 12:21:32 +0200

    Tests: new test for SkeletonAngles

---
+++ test/org/bonej/SkeletonAnglesTest.java
@@ -0,0 +1,33 @@
+package org.bonej;
+
+import static org.junit.Assert.*;
+import ij.ImagePlus;
+
+import org.doube.geometry.TestDataMaker;
+import org.junit.Test;
+
+public class SkeletonAnglesTest {
+
+	private final double pi4 = Math.PI / 4;
+	private final double pi2 = Math.PI / 2;
+	
+	private final double[][][] circleCrossResult = {{ 
+			{pi4, pi4, pi2},
+			null,
+			{pi4, pi2, pi4},
+			{pi4, pi2, pi4},
+			{pi4, pi4, pi2}
+		}};
+
+	@Test
+	public void testCalculateTriplePointAngles() {
+		ImagePlus imp = TestDataMaker.crossedCircle(256);
+		double[][][] result = (new SkeletonAngles())
+				.calculateTriplePointAngles(imp,
+						SkeletonAngles.VERTEX_TO_VERTEX);
+		for (int g = 0; g < circleCrossResult.length; g++)
+			for (int v = 0; v < circleCrossResult[g].length; v++)
+				assertArrayEquals(circleCrossResult[g][v], result[g][v], 1e-12);
+	}
+
+}
