c586d5df669563dff5a13938c5187a8335fa48de
Michael Doube <michael@doube.net>
2013-05-20 09:58:18 +0100

    Isosurface: add option to write binary STL straight to file
    Implemented the wrong way with code copy-paste, would need proper
    reimplementation by fiddling with the 3D viewer.

--- src/org/doube/bonej/MeasureSurface.java
+++ src/org/doube/bonej/MeasureSurface.java
@@ -18,6 +18,13 @@ package org.doube.bonej;
  *along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.List;
 
 import javax.vecmath.Color3f;
@@ -33,6 +40,7 @@ import ij.ImagePlus;
 import ij.gui.GenericDialog;
 import ij.plugin.PlugIn;
 import ij3d.Content;
+import ij3d.Executer;
 import ij3d.Image3DUniverse;
 
 import marchingcubes.MCTriangulator;
@@ -73,11 +81,13 @@ public class MeasureSurface implements PlugIn {
 		gd.addNumericField("Resampling", 6, 0);
 		gd.addNumericField("Threshold", threshold, 0);
 		gd.addCheckbox("Show surface", true);
+		gd.addCheckbox("Save as binary STL", false);
 		gd.addHelp("http://bonej.org/isosurface");
 		gd.showDialog();
 		int resamplingF = (int) Math.floor(gd.getNextNumber());
 		threshold = (int) Math.floor(gd.getNextNumber());
 		boolean doSurfaceRendering = gd.getNextBoolean();
+		boolean doBinarySTL = gd.getNextBoolean();
 		if (gd.wasCanceled())
 			return;
 
@@ -103,6 +113,10 @@ public class MeasureSurface implements PlugIn {
 		if (doSurfaceRendering) {
 			renderSurface(points, "Surface of " + imp.getTitle());
 		}
+		
+		if (doBinarySTL)
+			writeBinarySTL(points);
+		
 		UsageReporter.reportEvent(this).send();
 		return;
 	}
@@ -119,7 +133,7 @@ public class MeasureSurface implements PlugIn {
 
 		// Create a universe
 		Image3DUniverse univ = new Image3DUniverse();
-		
+
 		// Add the mesh
 		IJ.showStatus("Adding mesh to 3D viewer...");
 		Content c = univ.addCustomMesh(mesh, title);
@@ -128,8 +142,8 @@ public class MeasureSurface implements PlugIn {
 		c.setColor(green);
 		c.setTransparency((float) 0.33);
 		c.setSelected(true);
-		
-		//show the universe
+
+		// show the universe
 		IJ.showStatus("Displaying mesh in 3D viewer...");
 		univ.show();
 		IJ.showStatus("");
@@ -161,5 +175,68 @@ public class MeasureSurface implements PlugIn {
 			sumArea += deltaArea;
 		}
 		return sumArea;
+	}
+
+	public static void writeBinarySTL(List<Point3f> vertices) {
+		File stl_file = Executer.promptForFile("Save as binary STL",
+				"untitled", ".stl");
+		if (stl_file == null)
+			return;
+//		OutputStreamWriter dos = null;
+		DataOutputStream out = null;
+		try {
+		out = new DataOutputStream(new BufferedOutputStream(
+				new FileOutputStream(stl_file)));
+
+		String header = "Binary STL created by BoneJ.";
+		for (int i = header.length(); i < 80; i++) {
+			header = header + ".";
+		}
+		int triangles = vertices.size() / 3;
+		
+			out.writeBytes(header);
+			out.writeByte(triangles & 0xFF);
+			out.writeByte((triangles >> 8) & 0xFF);
+			out.writeByte((triangles >> 16) & 0xFF);
+			out.writeByte((triangles >> 24) & 0xFF);
+			for (int i = 0; i < vertices.size(); i += 3) {
+				Point3f p0 = vertices.get(i);
+				Point3f p1 = vertices.get(i + 1);
+				Point3f p2 = vertices.get(i + 2);
+				Point3f n = unitNormal(p0, p1, p2);
+				ByteBuffer bb = ByteBuffer.allocate(50);
+				bb.order(ByteOrder.LITTLE_ENDIAN);
+				bb.putFloat(n.x);
+				bb.putFloat(n.y);
+				bb.putFloat(n.z);
+				bb.putFloat(p0.x);
+				bb.putFloat(p0.y);
+				bb.putFloat(p0.z);
+				bb.putFloat(p1.x);
+				bb.putFloat(p1.y);
+				bb.putFloat(p1.z);
+				bb.putFloat(p2.x);
+				bb.putFloat(p2.y);
+				bb.putFloat(p2.z);
+				bb.putShort((short) 0);
+				out.write(bb.array());
+			}
+			out.flush();	
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private static Point3f unitNormal(Point3f p0, Point3f p1, Point3f p2) {
+		float nx = (p1.y-p0.y) * (p2.z-p0.z) - (p1.z-p0.z) * (p2.y-p0.y);
+		float ny = (p1.z-p0.z) * (p2.x-p0.x) - (p1.x-p0.x) * (p2.z-p0.z);
+		float nz = (p1.x-p0.x) * (p2.y-p0.y) - (p1.y-p0.y) * (p2.x-p0.x);
+		
+		float length = (float)Math.sqrt(nx * nx + ny * ny + nz* nz);
+		nx /= length;
+		ny /= length;
+		nz /= length;
+		return new Point3f(nx, ny, nz);
 	}
 }
