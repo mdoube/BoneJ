95f087b67abd2419d69eb285870671d613e08062
Michael Doube <michael@doube.net>
2013-07-29 17:16:06 +0100

    EllipsoidTest: add absic testing of unit sphere and 1,2,3 ellipsoid

--- test/org/doube/geometry/EllipsoidTest.java
+++ test/org/doube/geometry/EllipsoidTest.java
@@ -6,14 +6,25 @@ import org.junit.Test;
 
 public class EllipsoidTest {
 
+	/** Ellipsoid of radii = 1 and centred on origin */
+	Ellipsoid unitSphere = new Ellipsoid(1, 1, 1, 0, 0, 0, new double[][] {
+			{ 0, 0, 1 }, { 0, 1, 0 }, { 1, 0, 0 } });
+
+	/** Ellipsoid with radii of 1, 2, and 3, centred on (1,2,3) */
+	Ellipsoid oneTwoThree = FitEllipsoid.fitTo(FitEllipsoid.testEllipsoid(1, 2,
+			3, 0, 1, 2, 3, 0, 10000, true));
+
 	@Test
 	public void testGetVolume() {
-		fail("Not yet implemented");
+		assertEquals(Math.PI * 4 / 3, unitSphere.getVolume(), 1E-9);
 	}
 
 	@Test
 	public void testGetRadii() {
-		fail("Not yet implemented");
+		assertArrayEquals(new double[] { 1, 1, 1 }, unitSphere.getRadii(), 1E-9);
+
+		assertArrayEquals(new double[] { 3, 2, 1 }, oneTwoThree.getRadii(),
+				1E-9);
 	}
 
 	@Test
@@ -23,37 +34,103 @@ public class EllipsoidTest {
 
 	@Test
 	public void testContains() {
-		fail("Not yet implemented");
+		double[][] points = unitSphere.getSurfacePoints(1000);
+		Ellipsoid e = FitEllipsoid.fitTo(points);
+		double[][] vectors = Vectors.randomVectors(1000);
+
+		// inside
+		for (double[] v : vectors) {
+			double rand = Math.random();
+			double x = v[0] * rand;
+			double y = v[1] * rand;
+			double z = v[2] * rand;
+			assertTrue("Testing (" + x + ", " + y + ", " + z + ")",
+					e.contains(x, y, z));
+		}
+
+		// outside
+		for (double[] v : vectors) {
+			double rand = Math.random();
+			double x = v[0] / rand;
+			double y = v[1] / rand;
+			double z = v[2] / rand;
+			assertTrue("Testing (" + x + ", " + y + ", " + z + ")",
+					!e.contains(x, y, z));
+		}
+
+		assertTrue(
+				"Testing centroid (1, 2, 3) with solution = "
+						+ oneTwoThree.solve(1, 2, 3),
+				oneTwoThree.contains(1, 2, 3));
+
+		assertTrue("Testing wildly outside value with solution = "
+				+ oneTwoThree.solve(23, 213, 132),
+				!oneTwoThree.contains(23, 213, 132));
+
 	}
 
 	@Test
 	public void testSolve() {
-		fail("Not yet implemented");
+		double[][] points = unitSphere.getSurfacePoints(1000);
+		Ellipsoid e = FitEllipsoid.fitTo(points);
+		for (double[] p : points) {
+			assertEquals(1.0, e.solve(p[0], p[1], p[2]), 1E-9);
+		}
+		
+		points = oneTwoThree.getSurfacePoints(1000);
+		e = FitEllipsoid.fitTo(points);
+		for (double[] p : points) {
+			assertEquals(1.0, e.solve(p[0], p[1], p[2]), 1E-9);
+		}
+		
 	}
 
 	@Test
 	public void testGetMajorRadius() {
-		fail("Not yet implemented");
+		assertEquals(1, unitSphere.getMajorRadius(), 1E-9);
+
+		assertEquals(3, oneTwoThree.getMajorRadius(), 1E-9);
 	}
 
 	@Test
 	public void testGetCentre() {
-		fail("Not yet implemented");
+		assertArrayEquals(new double[] { 0, 0, 0 }, unitSphere.getCentre(),
+				1E-9);
+
+		assertArrayEquals(new double[] { 1, 2, 3 }, oneTwoThree.getCentre(),
+				1E-9);
 	}
 
 	@Test
 	public void testGetSurfacePoints() {
-		fail("Not yet implemented");
+		double[][] points = unitSphere.getSurfacePoints(10000);
+		for (double[] p : points) {
+			assertEquals(1, Trig.distance3D(p), 1E-9);
+		}
+
+		points = oneTwoThree.getSurfacePoints(10000);
+		for (double[] p : points) {
+			assertEquals(1, oneTwoThree.solve(p[0], p[1], p[2]), 1E-9);
+		}
+
 	}
 
 	@Test
 	public void testDilate() {
-		fail("Not yet implemented");
+		unitSphere.dilate(1);
+		double[][] points = unitSphere.getSurfacePoints(10000);
+		for (double[] p : points) {
+			assertEquals(2, Trig.distance3D(p), 1E-9);
+		}
 	}
 
 	@Test
 	public void testContract() {
-		fail("Not yet implemented");
+		unitSphere.contract(1.5);
+		double[][] points = unitSphere.getSurfacePoints(10000);
+		for (double[] p : points) {
+			assertEquals(0.5, Trig.distance3D(p), 1E-9);
+		}
 	}
 
 }
