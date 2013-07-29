eafc30191b012d7fe68a32d4113ec2b854133634
Michael Doube <michael@doube.net>
2013-07-29 12:19:17 +0100

    Ellipsoid: add JUnit test stubs

---
+++ test/org/doube/geometry/EllipsoidTest.java
@@ -0,0 +1,59 @@
+package org.doube.geometry;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EllipsoidTest {
+
+	@Test
+	public void testGetVolume() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRadii() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEquation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContains() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSolve() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMajorRadius() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCentre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSurfacePoints() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDilate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContract() {
+		fail("Not yet implemented");
+	}
+
+}
