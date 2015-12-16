9952a1ca5c2754a4e7784ed1db88122cfc987b32
Richard Domander <rdomander@rvc.ac.uk>
2015-12-16 10:38:31 +0000

    Add tests for ImageCheck.isBinary

--- test/org/doube/util/ImageCheckTest.java
+++ test/org/doube/util/ImageCheckTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import ij.process.ImageStatistics;
 import org.junit.Test;
 
 import ij.ImagePlus;
@@ -16,6 +17,9 @@ import ij.measure.Calibration;
  * Richard Domander
  */
 public class ImageCheckTest {
+    private static final int BINARY_WHITE = 0xFF;
+    private static final int BINARY_BLACK = 0x00;
+
     @Test
     public void testIsVoxelIsotropicReturnsFalseIfImageIsNull() throws Exception {
         boolean result = ImageCheck.isVoxelIsotropic(null);
@@ -47,5 +51,67 @@ public class ImageCheckTest {
 
         result = ImageCheck.isVoxelIsotropic(testImage, 1.0);
         assertFalse("Pixel depth too great to be anisotropic within tolerance", result);
+    }
+
+    @Test
+    public void testIsBinaryReturnsFalseIfImageIsNull() throws Exception {
+        boolean result = ImageCheck.isBinary(null);
+        assertFalse("Null image should not be binary", result);
+    }
+
+    @Test
+    public void testIsBinaryReturnsFalseIfImageHasWrongType() throws Exception {
+        ImagePlus testImage = mock(ImagePlus.class);
+        int wrongTypes[] = {ImagePlus.COLOR_256, ImagePlus.COLOR_RGB, ImagePlus.GRAY16, ImagePlus.GRAY32};
+
+        for (int wrongType : wrongTypes) {
+            when(testImage.getType()).thenReturn(wrongType);
+
+            boolean result = ImageCheck.isBinary(testImage);
+            assertFalse("Only ImagePlus.GRAY8 type should be binary", result);
+        }
+    }
+
+    @Test
+    public void testIsBinaryUsesHistogramCorrectly() throws Exception {
+        final int EIGHT_BYTES = 256;
+
+        // more than two colors
+        ImageStatistics nonBinaryStats = new ImageStatistics();
+        nonBinaryStats.pixelCount = 3;
+        nonBinaryStats.histogram = new int[EIGHT_BYTES];
+        nonBinaryStats.histogram[BINARY_BLACK] = 1;
+        nonBinaryStats.histogram[BINARY_WHITE] = 1;
+
+        ImagePlus testImage = mock(ImagePlus.class);
+        when(testImage.getStatistics()).thenReturn(nonBinaryStats);
+
+        boolean result = ImageCheck.isBinary(testImage);
+        assertFalse("Image with more than two colors must not be binary", result);
+
+        // wrong two colors
+        ImageStatistics wrongBinaryStats = new ImageStatistics();
+        wrongBinaryStats.pixelCount = 2;
+        wrongBinaryStats.histogram = new int[EIGHT_BYTES];
+        wrongBinaryStats.histogram[BINARY_BLACK] = 1;
+        wrongBinaryStats.histogram[BINARY_BLACK + 1] = 1;
+
+        when(testImage.getStatistics()).thenReturn(wrongBinaryStats);
+
+        result = ImageCheck.isBinary(testImage);
+        assertFalse("Image with wrong two colors (not " + BINARY_BLACK + " & " + BINARY_WHITE + ") must not be binary",
+                result);
+
+        // binary colors
+        ImageStatistics binaryStats = new ImageStatistics();
+        binaryStats.pixelCount = 2;
+        binaryStats.histogram = new int[EIGHT_BYTES];
+        binaryStats.histogram[BINARY_BLACK] = 1;
+        binaryStats.histogram[BINARY_WHITE] = 1;
+
+        when(testImage.getStatistics()).thenReturn(binaryStats);
+
+        result = ImageCheck.isBinary(testImage);
+        assertTrue("Image with two colors (" + BINARY_BLACK + " & " + BINARY_WHITE + ") should be binary", result);
     }
 }
